3176bea765dff06187b6c0b3ed0c025b6f18c75b
Vitaly <swp_91@inbox.ru>
2021-01-21 14:39:29 +0300

    fix 1 day, solution 2 days

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a >= b || a <= 0) {
+            System.out.println("некорректный ввод");
+        } else {
+            while (a < b) {
+                if (a % 5 == 0 && a % 10 != 0) {
+                    System.out.println(a);
+                }
+                a++;
+            }
+        }
     }
 }
