e4a526191a4b2c47dbbe12bdbe2384fe3f23ed09
Vitaly <swp_91@inbox.ru>
2021-01-23 00:59:33 +0300

    solution day 3.1/fix task 1 and task 2

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -5,33 +5,36 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String country = sc.nextLine();
-        switch (country) {
-            case "Москва":
-            case "Владивосток":
-            case "Ростов":
-                System.out.println("Россия");
-                break;
-            case "Рим":
-            case "Милан":
-            case "Турин":
-                System.out.println("Италия");
-                break;
-            case "Ливерпуль":
-            case "Манчестер":
-            case "Лондон":
-                System.out.println("Англия");
-                break;
-            case "Берлин":
-            case "Мюнхен":
-            case "Кёльн":
-                System.out.println("Германия");
-            case "Stop":
-                System.out.println("Программа завершила работу");
-                break;
-            default:
-                System.out.println("Неизвестная страна");
-
+        while (true) {
+            String country = sc.nextLine();
+           if (country.equals("Stop")){
+               break;
+           }else {
+           switch (country) {
+               case "Москва":
+               case "Владивосток":
+               case "Ростов":
+                   System.out.println("Россия");
+                   break;
+               case "Рим":
+               case "Милан":
+               case "Турин":
+                   System.out.println("Италия");
+                   break;
+               case "Ливерпуль":
+               case "Манчестер":
+               case "Лондон":
+                   System.out.println("Англия");
+                   break;
+               case "Берлин":
+               case "Мюнхен":
+               case "Кёльн":
+                   System.out.println("Германия");
+                   break;
+               default:
+                   System.out.println("Неизвестная страна");
+           }
+            }
         }
     }
 }
--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double dividend = sc.nextDouble();
-        double divisor = sc.nextDouble();
-        double result;
-        if (divisor != 0) {
-            result = dividend / divisor;
+        while (true) {
+            double num1 = sc.nextDouble();
+            double num2 = sc.nextDouble();
+            if (num2==0){
+                break;
+            }
+            double result = num1/num2;
             System.out.println(result);
         }
     }
