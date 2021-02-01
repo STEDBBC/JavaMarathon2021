2ae74a0e9cceffa124c8ee03c3e01633f0cd563a
Vitaly <swp_91@inbox.ru>
2021-02-01 22:42:15 +0300

    day 12 task 3,4,5. solutions

--- src/main/java/day12/task4/Task4.java
+++ src/main/java/day12/task4/Task4.java
@@ -1,7 +1,33 @@
 package day12.task4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task4 {
     public static void main(String[] args) {
+        List<String> members1 = new ArrayList<>();
+        members1.add("Chester Bennington");
+        members1.add("David Farrell");
+        members1.add("Bradford Delson");
+        members1.add("Joseph Hahn");
+        members1.add("Michael Shinoda");
+        MusicBand musicBand1 = new MusicBand("Linkin park", 1996, members1);
+        List<String> members2 = new ArrayList<>();
+        members2.add("Deryck Whibley");
+        members2.add("Jason McCaslin");
+        members2.add("Tom Thacker");
+        members2.add("Dave Baksh");
+        members2.add("Frank Zummo");
+        MusicBand musicBand2 = new MusicBand("Sum 41", 1996, members2);
+
+        musicBand1.listMembers();
+        musicBand2.listMembers();
+
+        MusicBand.transfer(musicBand1,musicBand2);
+
+        musicBand1.listMembers();
+        musicBand2.listMembers();
+
 
     }
 }
--- src/main/java/day12/task5/Task5.java
+++ src/main/java/day12/task5/Task5.java
@@ -1,7 +1,37 @@
 package day12.task5;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task5 {
     public static void main(String[] args) {
+        List <MusicArtist> members1 = new ArrayList<>();
+        members1.add(new MusicArtist("Chester Bennington",31));
+        members1.add(new MusicArtist("David Farrell",32));
+        members1.add(new MusicArtist("Bradford Delson",33));
+        members1.add(new MusicArtist("Joseph Hahn",34));
+        members1.add(new MusicArtist("Michael Shinoda",38));
+
+        MusicBand band1 = new MusicBand("linkin parl",1996,members1);
+
+        List <MusicArtist> members2 = new ArrayList<>();
+        members2.add(new MusicArtist("Deryck Whibley",22));
+        members2.add(new MusicArtist("Jason McCaslin",27));
+        members2.add(new MusicArtist("Tom Thacker",25));
+        members2.add(new MusicArtist("Dave Baksh",38));
+        members2.add(new MusicArtist("Frank Zummo",33));
+
+        MusicBand band2 = new MusicBand("sum 41",1996,members2);
+
+        band1.listMembers();
+        band2.listMembers();
+
+        MusicBand.transfer(band1,band2);
+
+        band1.listMembers();
+        band2.listMembers();
+
 
     }
 }
